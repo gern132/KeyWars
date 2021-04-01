0247ab0433a0544e04ea6f2c7bf18d3ca0e5689e
Aleksandr Lazarev <aleqqq132@gmail.com>
2021-04-01 10:53:50 +0300

    version 1.3

--- script.js
+++ script.js
@@ -10,19 +10,17 @@ function arrow() {  //step back
     document.querySelector('.game__level').classList.add('hidden');
     document.querySelector('.game__over').classList.add('hidden'); 
     document.querySelector('.level-up').classList.remove('hidden');
-    document.querySelector('.game__score').classList.remove('hidden');                                                                  // остановка программы(переделать)
+    document.querySelector('.game__score').classList.remove('hidden');  
+    clearInterval(time);                                                            // остановка программы(переделать)
 }
-
-function speed() { //choose speed
-
-}
+let time;
 
 function start() {  //strat game
     document.querySelector('.game__start').classList.add('hidden');
     document.querySelector('.game__level').classList.remove('hidden');
     document.querySelector('.level-up').classList.add('hidden');
     document.querySelector('.game__score').classList.add('hidden');
-
+    startCheck();
     let gamePole = document.querySelector('.game__level');
 
 let alfa = 'abcdefghijklmnopqrstuvwxyz';   //abcdefghijklmnopqrstuvwxyz
@@ -31,7 +29,8 @@ let arr = [];
 function clear() {
     let numberOfEnd = 5;
     if (arr.length >= numberOfEnd) {                             // кол-во разрешённых выводимых элементов
-        clearInterval(time);
+            clearInterval(time);
+            clearInterval(timer);
         document.querySelector('.game__over').addEventListener('click', () => {
             document.querySelector('.game__over').classList.add('hidden');
             document.querySelector('.game__start').classList.remove('hidden');
@@ -53,7 +52,7 @@ function clear() {
     }
 }
 
-let time = setInterval(() => {
+time = setInterval(() => {
     clear();
     console.log(arr);
 }, speedT);
@@ -73,20 +72,20 @@ let scorePole = document.querySelector('.score');
 let score = [];
 var speedT = 1000;
 function minusN() {
-    // console.log(speedT);
+    console.log(speedT);
     let sumN = score.reduce(function(prev, item) {
         return prev + item;
     }, 0);
 
     if(sumN <= 0) {
         for(i = 0; i < 1; i++) {
-            var addN = score.push(0);
-            speedT = speedT + 0;
+            let addN = score.push(0);
+            speedT = speedT;
         }
         scorePole.innerHTML = 0;
     }else {
         for(i = 0; i < 1; i++) {
-            var addN = score.push(-1);
+            let addN = score.push(-1);
             speedT = speedT + 100;
         }
         scorePole.innerHTML = sumN;
@@ -95,24 +94,42 @@ function minusN() {
 
 
 function plusN() {
-    // console.log(speedT);
+    console.log(speedT);
     let sumN = score.reduce(function(prev, item) {
         return prev + item;
     }, 0);
 
     if(sumN >= 9) {
         for(i = 0; i < 1; i++) {
-            var addN = score.push(0);
-            speedT = speedT - 0;
+            let addN = score.push(0);
+            speedT = speedT;
         }
         scorePole.innerHTML = 9;
     }else {
         for(i = 0; i < 1; i++) {
-            var addN = score.push(1);
+            let addN = score.push(1);
             speedT = speedT - 100;
         }
         scorePole.innerHTML = sumN;
     }
+}
+
+
+// timer
+
+let milliseconds = 0;
+let timer;
+let checkPole = document.querySelector('.game__time');
+
+function startCheck() {
+    timer = setInterval(() => {
+        milliseconds += 10;
+        let dataTime = new Date(milliseconds);
+        checkPole.innerHTML = 
+            ('0' + dataTime.getUTCMinutes()).slice(-2) + ':' +
+            ('0' + dataTime.getUTCSeconds()).slice(-2) + ':' +
+            ('0' + dataTime.getUTCMilliseconds()).slice(-3,-1);
+    }, 10)
 }
 
 
